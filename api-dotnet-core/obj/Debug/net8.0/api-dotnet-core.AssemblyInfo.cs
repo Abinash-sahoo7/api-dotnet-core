41f77599e8bd9d7978b7284c732c13639e14addc
sahooabinash2003@gmail.com <avinashsahoo2021@gmail.com>
2025-11-22 01:05:45 +0530

    dll changes dotnet

--- api-dotnet-core/obj/Debug/net8.0/api-dotnet-core.AssemblyInfo.cs
+++ api-dotnet-core/obj/Debug/net8.0/api-dotnet-core.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api-dotnet-core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a13d28a831e25a55d561d8d603355c8f54a91dca")]
 [assembly: System.Reflection.AssemblyProductAttribute("api-dotnet-core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api-dotnet-core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
